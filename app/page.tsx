ac89b0fdc6ab6c1c16d5a5a4791bc101dabdeb3a
Marion MICHEL <marion@air-de-marion.home>
2026-03-02 09:55:42 +0100

    Fix JSX comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <!-- Ton app Antigravity sera ici -->
+      {/* Ton app Antigravity sera ici */}
       <h1>Mon app Antigravity sera ici !</h1>
     </div>
   )
